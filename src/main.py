6459fe613d69ef28d0284cac15f8cb0c7da9536b
Guilherme Torres <guilhermetorres97@gmail.com>
2017-09-12 11:29:16 -0300

    input stuff

--- src/main.py
+++ src/main.py
@@ -2,17 +2,16 @@ import sys
 import random
 import csv
 
-if len(sys.argv) != 2:
-    print("usage: python main.py <csv test file>")
-    sys.exit()
+def main():
+    if len(sys.argv) != 2:
+        print("usage: python main.py <csv test file>")
+        sys.exit()
 
-print("main")
-with open(sys.argv[1], 'rb') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#    i = 0
-#    for row in spamreader:
-#        print i, ':'
-#        print ', '.join(row)
-#        i += 1
-    for row in reader:
-        print (row)
+    print("main")
+    csvfile = (sys.argv[1], newline = '')
+    reader = csv.reader(csvfile, delimiter=' ', quotechat='|')
+    for row in reader.split('\n'):
+        print(row)
+
+if __name__ == '__main__':
+    main()
--- src/test0.sh
+++ src/test0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 -m py_compile main.py
-chmod 755 main.pyc
-./main.pyc ../datasets/house-train.csv
+chmod 755 ./__pycache__/main.cpython-35.pyc
+./__pycache__/main.cpython-35.pyc ../datasets/house-train.csv
