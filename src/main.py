d5b5c1af23791fa0837347b06ed0eba66afcff43
Guilherme Torres <guilhermetorres97@gmail.com>
2017-09-14 18:43:54 -0300

    input values are now saved in RAM

--- src/main.py
+++ src/main.py
@@ -2,16 +2,21 @@ import sys
 import random
 import csv
 
+class Data():
+    def __init__(self, filename):
+        with open(filename, "r") as csvfile:
+            csvinput = csv.reader(csvfile)
+            self.details = list(csvinput)
+    def get_value(self, row, col):
+        return self.details[row][col]
+
 def main():
     if len(sys.argv) != 2:
         print("usage: python main.py <csv test file>")
         sys.exit()
 
     print("main")
-    csvfile = (sys.argv[1], newline = '')
-    reader = csv.reader(csvfile, delimiter=' ', quotechat='|')
-    for row in reader.split('\n'):
-        print(row)
+    train_data = Data(sys.argv[1])
 
 if __name__ == '__main__':
     main()
