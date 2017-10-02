27d24139e602a614400d99025c6211ca7152795f
Guilherme Torres <guilhermetorres97@gmail.com>
2017-10-01 22:31:45 -0300

    programa preparado

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cn-tp1-gp
 Genetic Programming learning algorithm
+
+Requerimentos para executar:
+- Python 3.5+
+
+Execute o test.sh na pasta /src para rodar o programa.
+Insira como argumentos apenas os nomes dos arquivos de treino e teste, nessa ordem.
+O programa realiza 30 iterações do algoritmo e gera um arquivo exp.log, onde a cada geração o output dado é a melhor fitness, a pior e a média, respectivamente.
--- src/test0.sh
+++ src/test0.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-python3 main.py ../datasets/keijzer-7-train.csv ../datasets/keijzer-7-test.csv 50 50 0.6 0.3 2 > exp0.log
-python3 main.py ../datasets/keijzer-7-train.csv ../datasets/keijzer-7-test.csv 50 50 0.6 0.3 3 > exp1.log
-python3 main.py ../datasets/keijzer-7-train.csv ../datasets/keijzer-7-test.csv 50 50 0.6 0.3 4 > exp2.log
-python3 main.py ../datasets/keijzer-7-train.csv ../datasets/keijzer-7-test.csv 50 50 0.6 0.3 5 > exp3.log
-python3 main.py ../datasets/house-train.csv ../datasets/house-test.csv 50 50 0.6 0.3 2 > ../test-logs/house/tournament/exp0.log
-python3 main.py ../datasets/house-train.csv ../datasets/house-test.csv 50 50 0.6 0.3 3 > ../test-logs/house/tournament/exp1.log
-#python3 -m py_compile main.py
-#chmod 755 ./__pycache__/main.cpython-35.pyc
-#./__pycache__/main.cpython-35.pyc ../datasets/house-train.csv ../datasets/house-test.csv 500 2500 0.9 0.05 2
+python3 main.py $1 $2 50 50 0.8 0.3 3 > exp.log
